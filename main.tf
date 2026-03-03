e23664e2fcbc4b558cf648f20571b8dc2ec92ad5
saikumar yadav <saikumargumpana@gmail.com>
2026-03-03 11:31:44 +0530

    Added workspace support and lock file

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 provider "aws" {
   region = var.region
 }
-
-
-
-
 resource "aws_instance" "web" {
   count         = var.instance_count
   ami           = var.ami
-  instance_type = var.instance_types[2]
-  tags          = var.instance_tags
+  instance_type = terraform.workspace == "prod" ? "t3.medium" : "t2.micro"
+  tags = merge(
+    var.instance_tags,
+    {
+      Environment = terraform.workspace
+      Name        = "web-${terraform.workspace}"
+    }
+  )
+
 
 
 
